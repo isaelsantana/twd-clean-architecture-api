efa1a3772c1bedc3009f75b45fb3171035b83e6d
Isael <isaelsantana.silva@gmail.com>
2022-03-29 23:51:21 -0300

    feat: validate local domain larger than 255 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -2,8 +2,8 @@ export class Email {
   static validate (email: string): boolean {
     if (!email) { return false }
 
-    const [local] = email.split('@')
-    if (local.length > 64 || email.length > 320) { return false }
+    const [local, domain] = email.split('@')
+    if (local.length > 64 || email.length > 320 || domain.length > 255) { return false }
     return true
   }
 }
