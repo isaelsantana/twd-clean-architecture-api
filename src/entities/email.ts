6721858420e98b8fd63958afb730de01f0b97a28
Isael <isaelsantana.silva@gmail.com>
2022-04-18 20:55:55 -0300

    feat: validate email with domain part with invalid char

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,6 +1,9 @@
 export class Email {
   static validate (email: string): boolean {
+    const emailRegex = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
     if (!email) { return false }
+
+    if (!emailRegex.test(email)) return false
 
     const [local, domain] = email.split('@')
     const isdomainPartsInvalidLength = domain.split('.').some(item => item.length > 63)
