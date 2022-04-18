fd0116efe2f8e7ad08b36376fdddcf2d02af36ec
Isael <isaelsantana.silva@gmail.com>
2022-04-18 20:39:12 -0300

    feat: validate domain email with domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -3,7 +3,8 @@ export class Email {
     if (!email) { return false }
 
     const [local, domain] = email.split('@')
-    if (local.length > 64 || local.length === 0 || email.length > 320 || domain.length > 255 || domain.length === 0) { return false }
+    const isdomainPartsInvalidLength = domain.split('.').some(item => item.length > 63)
+    if (local.length > 64 || local.length === 0 || email.length > 320 || domain.length > 255 || domain.length === 0 || isdomainPartsInvalidLength) { return false }
     return true
   }
 }
